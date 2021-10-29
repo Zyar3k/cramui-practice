d5828f94ed333e0e7790b45b58d3258ecdb20ba3
zyar3k <a.zygan@gmail.com>
2021-10-29 16:25:05 +0200

    Add grid system

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import { AppBar, Toolbar } from "@material-ui/core";
+import { AppBar, Grid, Toolbar } from "@material-ui/core";
+import Feed from "./components/Feed";
+import Leftbar from "./components/Leftbar";
 import Navbar from "./components/Navbar";
+import Rightbar from "./components/Rightbar";
 
 function App() {
   return (
     <div>
       <Navbar />
-      
-      <h1>Start MuI</h1>
+      <Grid container>
+        <Grid item sm={2}>
+          <Leftbar />
+        </Grid>
+        <Grid item sm={7}>
+          <Feed />
+        </Grid>
+        <Grid item sm={3}>
+          <Rightbar />
+        </Grid>
+      </Grid>
     </div>
   );
 }
---
+++ src/components/Leftbar.js
@@ -0,0 +1,38 @@
+import { Container, makeStyles, Typography } from "@material-ui/core";
+import { Home } from "@material-ui/icons";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    paddingTop: theme.spacing(10),
+  },
+}));
+
+const Leftbar = () => {
+  const classes = useStyles();
+  return (
+    <Container className={classes.container}>
+      <div className={classes.item}>
+        <Home className={classes.icon} />
+        <Typography className={classes.text}>Homepage</Typography>
+      </div>
+      <div className={classes.item}>
+        <Home className={classes.icon} />
+        <Typography className={classes.text}>Homepage</Typography>
+      </div>
+      <div className={classes.item}>
+        <Home className={classes.icon} />
+        <Typography className={classes.text}>Homepage</Typography>
+      </div>
+      <div className={classes.item}>
+        <Home className={classes.icon} />
+        <Typography className={classes.text}>Homepage</Typography>
+      </div>
+      <div className={classes.item}>
+        <Home className={classes.icon} />
+        <Typography className={classes.text}>Homepage</Typography>
+      </div>
+    </Container>
+  );
+};
+
+export default Leftbar;
